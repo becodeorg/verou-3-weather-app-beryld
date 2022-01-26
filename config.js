ec25f40cc79f011ed60bf22d8806beaf6e640e33
beryld <deback.beryl@gmail.com>
2022-01-26 13:46:08 +0100

    style : add background image per researchs and style the small screen page

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const data = {
     key : '7be3737d498e045a3e644d252365123e',
-    keyhere : 'HL5Wf8jfhXR8H0ucVXS1GJpFFXopWAITBCZ3jX8oIx4'
+    keyhere : 'HL5Wf8jfhXR8H0ucVXS1GJpFFXopWAITBCZ3jX8oIx4',
+    unsplkey : '53pwftuU-Hne0C71r8jjZLCnCL5To-B2aj0Be7WRJFw'
 }
--- meteo.js
+++ meteo.js
@@ -1,38 +1,31 @@
 import { data } from "./config.js";
-const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ]
-const cityInput = document.getElementById("city");
-const cityButton = document.getElementById("cityButt");
-const titleCityResult = document.getElementById("titleCity");
-const today = document.getElementById("current");
-const fiveOnes = document.getElementById("fiveDays");
-const dating = document.getElementById('today-date')
-const cityName = document.getElementById('name-city')
-const dayOneName = document.getElementById('day-one')
-const dayOneTemp = document.getElementById('temp-one')
-const dayTwoName = document.getElementById('day-two')
-const dayTwoTemp = document.getElementById('temp-two')
-const dayThreeName = document.getElementById('day-three')
-const dayThreeTemp = document.getElementById('temp-three')
-const dayFourName = document.getElementById('day-four')
-const dayFourTemp = document.getElementById('temp-four')
-const dayOneIcon = document.getElementById('icon-one')
-const dayTwoIcon  = document.getElementById('icon-two')
-const dayThreeIcon = document.getElementById('icon-three')
-const dayFourIcon = document.getElementById('icon-four')
-const dayOneHum = document.getElementById('humidity-one')
-const dayTwoHum = document.getElementById('humidity-two')
-const dayThreeHum = document.getElementById('humidity-three')
-const dayFourHum = document.getElementById('humidity-four')
-//Hot to have the date dd/mm/yyyy :
-var todayla = new Date;
-var days = todayla.getDay()+1;
-console.log(days);
-var currentDate = String(todayla.getDate())+'/0'+ String(todayla.getMonth()+1) +"/" + String(todayla.getFullYear());
-let lat;
-let lon;
-let nameCity;
-
-
+const {
+  cityButton,
+  cityInput,
+  dating,
+  cityName,
+  today,
+  dayOneName,
+  daysOfWeek,
+  dayOneTemp,
+  dayTwoName,
+  dayTwoTemp,
+  dayThreeName,
+  dayThreeTemp,
+  dayFourName,
+  dayFourTemp,
+  dayOneIcon,
+  dayTwoIcon,
+  dayThreeIcon,
+  dayFourIcon,
+  dayOnepop,
+  dayTwopop,
+  dayThreepop,
+  dayFourpop,
+  bodyback,
+  hideIt,
+} = consts();
+var { lat, lon, nameCity, currentDate, days } = vars();
 
 cityButton.onclick = () => {
   fetch(
@@ -50,11 +43,12 @@ cityButton.onclick = () => {
     .then(function (responseLat) {
       lat = responseLat[0].lat;
       lon = responseLat[0].lon;
-      nameCity = responseLat[0].name
+      nameCity = responseLat[0].name;
       console.log(nameCity);
       console.log(lon);
       console.log(lat);
       fetchmeteo();
+      imgFishing();
     });
 
   async function fetchmeteo() {
@@ -75,43 +69,138 @@ cityButton.onclick = () => {
         return response.json();
       })
       .then(function (response) {
-          dating.textContent= currentDate
-        cityName.textContent=nameCity
-        today.textContent =
-         parseInt(response.current.temp) + "°"  ;
+        //TODO: Should I put the date ? 
+        // dating.textContent = currentDate;
+        cityName.textContent = nameCity;
+        today.textContent = parseInt(response.current.temp) + "";
         console.log(cityInput.value);
-        
-        
-        dayOneName.textContent= daysOfWeek[days-1] ;
-        dayOneTemp.textContent=  parseInt(response.daily[1].temp.day) + "°";
+
+        dayOneName.textContent = daysOfWeek[days - 1];
+        dayOneTemp.textContent = parseInt(response.daily[1].temp.day) + "°";
         dayTwoName.textContent = daysOfWeek[days];
-        dayTwoTemp.textContent=  parseInt(response.daily[2].temp.day);
-        dayThreeName.textContent = daysOfWeek[days+1];
-        dayThreeTemp.textContent=  parseInt(response.daily[3].temp.day);
-        dayFourName.textContent = daysOfWeek[days+2];
-        dayFourTemp.textContent=  parseInt(response.daily[4].temp.day);
-        
-        dayOneIcon.src="https://openweathermap.org/img/wn/"+ response.daily[1].weather[0].icon +"@2x.png";
-        dayTwoIcon.src="https://openweathermap.org/img/wn/"+ response.daily[2].weather[0].icon +"@2x.png";
-        dayThreeIcon.src="https://openweathermap.org/img/wn/"+ response.daily[3].weather[0].icon +"@2x.png";
-        dayFourIcon.src="https://openweathermap.org/img/wn/"+ response.daily[4].weather[0].icon +"@2x.png";
-        
+        dayTwoTemp.textContent = parseInt(response.daily[2].temp.day) + "°";
+        dayThreeName.textContent = daysOfWeek[days + 1];
+        dayThreeTemp.textContent = parseInt(response.daily[3].temp.day) + "°";
+        dayFourName.textContent = daysOfWeek[days + 2];
+        dayFourTemp.textContent = parseInt(response.daily[4].temp.day) + "°";
 
+        dayOneIcon.src =
+          "https://openweathermap.org/img/wn/" +
+          response.daily[1].weather[0].icon +
+          "@2x.png";
+        dayTwoIcon.src =
+          "https://openweathermap.org/img/wn/" +
+          response.daily[2].weather[0].icon +
+          "@2x.png";
+        dayThreeIcon.src =
+          "https://openweathermap.org/img/wn/" +
+          response.daily[3].weather[0].icon +
+          "@2x.png";
+        dayFourIcon.src =
+          "https://openweathermap.org/img/wn/" +
+          response.daily[4].weather[0].icon +
+          "@2x.png";
 
-    });
+        dayOnepop.textContent = response.daily[1].pop * 100 + " %";
+        dayTwopop.textContent = response.daily[2].pop * 100 + " %";
+        dayThreepop.textContent = response.daily[3].pop * 100 + " %";
+        dayFourpop.textContent = response.daily[4].pop * 100 + " %";
+        hideIt.style.visibility="visible";
+      });
   }
-
+  async function imgFishing() {
+    fetch(
+      "https://api.unsplash.com/search/photos?query=" +
+        cityInput.value +
+        "&per_page=20&client_id=" +
+        data.unsplkey
+    )
+      .then(function (unsplResponse) {
+        if (!unsplResponse.ok) {
+          throw new Error(
+            `HTTP error! What did you try to do ? status: ${unsplResponse.status}`
+          );
+        }
+        return unsplResponse.json();
+      })
+      //If style not backgroundimage but just background the cover property wont work !!!! 
+      .then(function (unsplResponse) {
+        bodyback.style.backgroundImage =
+          "url('" + unsplResponse.results[0].urls.small + ")";
+      });
+  }
 };
 
+//TODO: fetch unix time stamp from API to display current time of city researched
+function vars() {
+  var todayla = new Date();
+  console.log(todayla);
+  var days = todayla.getDay() + 1;
+  console.log(days);
+  var currentDate =
+    String(todayla.getDate()) +
+    "/0" +
+    String(todayla.getMonth() + 1) +
+    "/" +
+    String(todayla.getFullYear());
+  let lat;
+  let lon;
+  let nameCity;
+  return { lat, lon, nameCity, currentDate, days };
+}
 
-
-// function toDate() {
-//     let unix_today = response.current.dt; // fetch date in seconds
-//     var date_in_mlsec = new Date(unix_today * 1000) //convert to milliseconds
-//     var date = date_in_mlsec.getDate()
-//     var hours = date_in_mlsec.getHours() //use gethours method to fetch the hour element 
-//     var minutes = "0" + date_in_mlsec.getMinutes();
-//     var seconds = "0" + date_in_mlsec.getSeconds();
-//     console.log(date);
-// }
-// toDate()
+function consts() {
+  
+  const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const cityInput = document.getElementById("city");
+  const cityButton = document.getElementById("cityButt");
+  const titleCityResult = document.getElementById("titleCity");
+  const today = document.getElementById("current");
+  const fiveOnes = document.getElementById("fiveDays");
+  const dating = document.getElementById("today-date");
+  const cityName = document.getElementById("name-city");
+  const dayOneName = document.getElementById("day-one");
+  const dayOneTemp = document.getElementById("temp-one");
+  const dayTwoName = document.getElementById("day-two");
+  const dayTwoTemp = document.getElementById("temp-two");
+  const dayThreeName = document.getElementById("day-three");
+  const dayThreeTemp = document.getElementById("temp-three");
+  const dayFourName = document.getElementById("day-four");
+  const dayFourTemp = document.getElementById("temp-four");
+  const dayOneIcon = document.getElementById("icon-one");
+  const dayTwoIcon = document.getElementById("icon-two");
+  const dayThreeIcon = document.getElementById("icon-three");
+  const dayFourIcon = document.getElementById("icon-four");
+  const dayOnepop = document.getElementById("popidity-one");
+  const dayTwopop = document.getElementById("popidity-two");
+  const dayThreepop = document.getElementById("popidity-three");
+  const dayFourpop = document.getElementById("popidity-four");
+  const bodyback = document.getElementById('bodybackA');
+  const hideIt = document.getElementById('container-results');
+  return {
+    cityButton,
+    cityInput,
+    dating,
+    cityName,
+    today,
+    dayOneName,
+    daysOfWeek,
+    dayOneTemp,
+    dayTwoName,
+    dayTwoTemp,
+    dayThreeName,
+    dayThreeTemp,
+    dayFourName,
+    dayFourTemp,
+    dayOneIcon,
+    dayTwoIcon,
+    dayThreeIcon,
+    dayFourIcon,
+    dayOnepop,
+    dayTwopop,
+    dayThreepop,
+    dayFourpop,
+    bodyback,
+    hideIt,
+  };
+}
