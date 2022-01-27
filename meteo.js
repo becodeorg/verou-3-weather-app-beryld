fdf13a42d67146bb7c715ff9fcd46658ebd414cb
beryld <deback.beryl@gmail.com>
2022-01-27 09:40:42 +0100

    Build : translate the  icons and temperatures logs into for loops

--- meteo.js
+++ meteo.js
@@ -3,21 +3,12 @@ const {
   cityButton,
   cityInput,
   dating,
+  dayNames,
   cityName,
   today,
-  dayOneName,
   daysOfWeek,
-  dayOneTemp,
-  dayTwoName,
-  dayTwoTemp,
-  dayThreeName,
-  dayThreeTemp,
-  dayFourName,
-  dayFourTemp,
-  dayOneIcon,
-  dayTwoIcon,
-  dayThreeIcon,
-  dayFourIcon,
+  temperat,
+  icons,
   daypops,
   bodyback,
   hideIt,
@@ -41,9 +32,6 @@ cityButton.onclick = () => {
       lat = responseLat[0].lat;
       lon = responseLat[0].lon;
       nameCity = responseLat[0].name;
-      console.log(nameCity);
-      console.log(lon);
-      console.log(lat);
       fetchmeteo();
       imgFishing();
     });
@@ -66,48 +54,34 @@ cityButton.onclick = () => {
         return response.json();
       })
       .then(function (response) {
-        //TODO: Should I put the date ? 
+        //TODO: Should I put the date ?
         // dating.textContent = currentDate;
         cityName.textContent = nameCity;
         today.textContent = parseInt(response.current.temp) + "";
-        console.log(cityInput.value);
 
-        //TODO: FOR LOOP
         
-        dayOneName.textContent = daysOfWeek[days - 1];
-        dayOneTemp.textContent = parseInt(response.daily[1].temp.day) + "°";
-        dayTwoName.textContent = daysOfWeek[days];
-        dayTwoTemp.textContent = parseInt(response.daily[2].temp.day) + "°";
-        dayThreeName.textContent = daysOfWeek[days + 1];
-        dayThreeTemp.textContent = parseInt(response.daily[3].temp.day) + "°";
-        dayFourName.textContent = daysOfWeek[days + 2];
-        dayFourTemp.textContent = parseInt(response.daily[4].temp.day) + "°";
-
-        //TODO: FOR LOOP
-        dayOneIcon.src =
-          "https://openweathermap.org/img/wn/" +
-          response.daily[1].weather[0].icon +
-          "@2x.png";
-        dayTwoIcon.src =
-          "https://openweathermap.org/img/wn/" +
-          response.daily[2].weather[0].icon +
-          "@2x.png";
-        dayThreeIcon.src =
-          "https://openweathermap.org/img/wn/" +
-          response.daily[3].weather[0].icon +
-          "@2x.png";
-        dayFourIcon.src =
-          "https://openweathermap.org/img/wn/" +
-          response.daily[4].weather[0].icon +
-          "@2x.png";
-          /*dayOnepop.textContent = parseInt(response.daily[1].pop * 100) + " %";
-          dayTwopop.textContent = parseInt(response.daily[2].pop * 100 )+ " %";
-          dayThreepop.textContent = parseInt(response.daily[3].pop * 100 )+ " %";
-          dayFourpop.textContent = parseInt( response.daily[4].pop * 100) + " %"; */
-          for (let index = 0; index < 4; index++) {
-            daypops[index].textContent= parseInt(response.daily[index+1].pop *100) + " %"
+        
+        for (let i = 0; i < 4; i++) {
+          let dayMax = days - 1 + i;
+            if (dayMax >= 7) {
+              dayMax = dayMax - 7;
             }
-          hideIt.style.visibility="visible";
+              dayNames[i].textContent = daysOfWeek[dayMax];
+              temperat[i].textContent = parseInt(response.daily[i + 1].temp.day) + "°";
+        }
+
+        for (let i = 0; i < 4; i++) {
+          
+          icons[i].src =
+            "https://openweathermap.org/img/wn/" +
+            response.daily[i + 1].weather[0].icon +
+            "@2x.png";
+        }
+        for (let index = 0; index < 4; index++) {
+          daypops[index].textContent =
+            parseInt(response.daily[index + 1].pop * 100) + " %";
+        }
+        hideIt.style.visibility = "visible";
       });
   }
   async function imgFishing() {
@@ -125,7 +99,7 @@ cityButton.onclick = () => {
         }
         return unsplResponse.json();
       })
-      //If style not backgroundimage but just background the cover property wont work !!!! 
+      //If style not backgroundimage but just background the cover property wont work !!!!
       .then(function (unsplResponse) {
         bodyback.style.backgroundImage =
           "url('" + unsplResponse.results[0].urls.small + ")";
@@ -152,7 +126,6 @@ function vars() {
 }
 
 function consts() {
-  
   const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   const cityInput = document.getElementById("city");
   const cityButton = document.getElementById("cityButt");
@@ -161,42 +134,60 @@ function consts() {
   const fiveOnes = document.getElementById("fiveDays");
   const dating = document.getElementById("today-date");
   const cityName = document.getElementById("name-city");
-  const dayOneName = document.getElementById("day-one");
-  const dayOneTemp = document.getElementById("temp-one");
-  const dayTwoName = document.getElementById("day-two");
-  const dayTwoTemp = document.getElementById("temp-two");
-  const dayThreeName = document.getElementById("day-three");
-  const dayThreeTemp = document.getElementById("temp-three");
-  const dayFourName = document.getElementById("day-four");
-  const dayFourTemp = document.getElementById("temp-four");
-  const dayOneIcon = document.getElementById("icon-one");
-  const dayTwoIcon = document.getElementById("icon-two");
-  const dayThreeIcon = document.getElementById("icon-three");
-  const dayFourIcon = document.getElementById("icon-four");
-  const daypops = document.querySelectorAll('.popic');
-  const bodyback = document.getElementById('bodybackA');
-  const hideIt = document.getElementById('container-results');
+  const daypops = document.querySelectorAll(".popic");
+  const bodyback = document.getElementById("bodybackA");
+  const hideIt = document.getElementById("container-results");
+  const dayNames = document.querySelectorAll(".day-name");
+  const temperat = document.querySelectorAll(".temperature");
+  const icons = document.querySelectorAll(".ic");
   return {
     cityButton,
     cityInput,
     dating,
     cityName,
     today,
-    dayOneName,
+    dayNames,
     daysOfWeek,
-    dayOneTemp,
-    dayTwoName,
-    dayTwoTemp,
-    dayThreeName,
-    dayThreeTemp,
-    dayFourName,
-    dayFourTemp,
-    dayOneIcon,
-    dayTwoIcon,
-    dayThreeIcon,
-    dayFourIcon,
+    temperat,
     bodyback,
     hideIt,
     daypops,
+    icons,
   };
 }
+/*
+          dayOneName.textContent = daysOfWeek[days - 1];
+          dayTwoName.textContent = daysOfWeek[days];
+          dayThreeName.textContent = daysOfWeek[days + 1];
+          dayFourName.textContent = daysOfWeek[days + 2];
+
+          dayOneTemp.textContent = parseInt(response.daily[1].temp.day) + "°";
+          dayTwoTemp.textContent = parseInt(response.daily[2].temp.day) + "°";
+          dayThreeTemp.textContent = parseInt(response.daily[3].temp.day) + "°";
+          dayFourTemp.textContent = parseInt(response.daily[4].temp.day) + "°";
+          
+          dayOnepop.textContent = parseInt(response.daily[1].pop * 100) + " %";
+          dayTwopop.textContent = parseInt(response.daily[2].pop * 100 )+ " %";
+          dayThreepop.textContent = parseInt(response.daily[3].pop * 100 )+ " %";
+          dayFourpop.textContent = parseInt( response.daily[4].pop * 100) + " %"; 
+
+          
+        dayOneIcon.src =
+        "https://openweathermap.org/img/wn/" +
+        response.daily[1].weather[0].icon +
+        "@2x.png";
+        
+        dayTwoIcon.src =
+        "https://openweathermap.org/img/wn/" +
+        response.daily[2].weather[0].icon +
+        "@2x.png";
+        
+        dayThreeIcon.src =
+        "https://openweathermap.org/img/wn/" +
+        response.daily[3].weather[0].icon +
+        "@2x.png";
+        
+        dayFourIcon.src =
+        "https://openweathermap.org/img/wn/" +
+        response.daily[4].weather[0].icon +
+        "@2x.png";*/
