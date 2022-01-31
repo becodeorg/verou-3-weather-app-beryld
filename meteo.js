13054baabca90b17eebba1e0f2d21ae5ad012a16
beryld <deback.beryl@gmail.com>
2022-01-31 15:45:48 +0100

    Format : delete non-necessary consts and access them more directly inside functions

--- meteo.js
+++ meteo.js
@@ -5,16 +5,12 @@ const {
   dayNames,
   cityName,
   today,
-  daysOfWeek,
-  temperat,
   icons,
-  daypops,
-  bodyback,
+  
   feet,
 } = consts();
 var { lat, lon, nameCity, currentDate, days } = vars();
 
-//TODO: Format onclick inside a function that is call - able
 
 cityInput.value = "Taiwan";
 
@@ -35,6 +31,9 @@ const fetchCoordinates = () => {
       }
       return responseLat.json();
     })
+    .catch(error => { 
+      console.log(error)
+    })  
     .then(function (responseLat) {
       lat = responseLat[0].lat;
       lon = responseLat[0].lon;
@@ -52,8 +51,11 @@ const fetchMeteo = () => {
       }
       return response.json();
     })
+    .catch(error => { 
+      console.log(error)
+    })  
     .then(function (response) {
-      const hideIt = document.getElementById("container-results");
+      // const hideIt = document.getElementById("container-results");
       cityName.textContent = nameCity;
       today.textContent = parseInt(response.current.temp) + "";
       fetchNameAndTemperature(response);
@@ -66,7 +68,7 @@ const fetchMeteo = () => {
 
 const fetchPrecip = (response) => {
   for (let index = 0; index < 4; index++) {
-    daypops[index].textContent = parseInt(response.daily[index + 1].pop * 100) + " %";
+    document.querySelectorAll(".popic")[index].textContent = parseInt(response.daily[index + 1].pop * 100) + " %";
   }
 };
 
@@ -88,21 +90,26 @@ const imgFishing = () => {
       }
       return unsplashResponse.json();
     })
+    .catch(error => { 
+      console.log(error)
+    })  
     //If style not backgroundimage but just background the cover property wont work !!!!
     .then(function (unsplashResponse) {
       const imgArraySize = unsplashResponse.results.length;
       const choise = Math.floor(Math.random() * imgArraySize);
-      bodyback.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, .7), rgba(255,255,255,0.5)), " + "url('" + unsplashResponse.results[choise - 1].urls.small + ")";
+      document.getElementById("bodybackA").style.backgroundImage = "linear-gradient(rgba(255, 255, 255, .7), rgba(255,255,255,0.5)), " + "url('" + unsplashResponse.results[choise - 1].urls.small + ")";
       feet.textContent = "Unsplash  license : " + unsplashResponse.results[choise - 1].user.name + "\n" + unsplashResponse.results[choise - 1].user.links.html; });
+   
 };
 
 const fetchNameAndTemperature = (response) => {for (let i = 0; i < 4; i++) {
+  const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   let dayMax = days - 1 + i;
   if (dayMax >= 7) {
       dayMax = dayMax - 7;
   }
   dayNames[i].textContent = daysOfWeek[dayMax];
-  temperat[i].textContent = parseInt(response.daily[i + 1].temp.day) + "°";
+  document.querySelectorAll(".temperature")[i].textContent = parseInt(response.daily[i + 1].temp.day) + "°";
 }
 
 };
@@ -124,29 +131,22 @@ function vars() {
 }
 
 function consts() {
-  const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  
   const cityInput = document.getElementById("city");
   const searchButton = document.getElementById("cityButt");
   const today = document.getElementById("current");
   const cityName = document.getElementById("name-city");
-  const daypops = document.querySelectorAll(".popic");
-  const bodyback = document.getElementById("bodybackA");
   
   const dayNames = document.querySelectorAll(".day-name");
-  const temperat = document.querySelectorAll(".temperature");
   const icons = document.querySelectorAll(".ic");
   const feet = document.getElementById("foot");
   return {
-    daysOfWeek,
     cityInput,
     searchButton,
     today,
     cityName,
-    daypops,
-    bodyback,
     
     dayNames,
-    temperat,
     icons,
     feet,
   };
